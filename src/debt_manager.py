171910650a223e624c4bb469d96b76857744e9ec
yatoyun <kaneko.yuta0813@gmail.com>
2023-07-29 14:06:27 +0900

    temporary finish debt_manager

--- src/debt_manager.py
+++ src/debt_manager.py
@@ -1,21 +1,35 @@
 from src.dynamo_db import DynamoDB
 
 class DebtManager:
-    def __init__(self):
-        self.dynamo_db = DynamoDB()
+    def __init__(self, table_name):
+        self.dynamo_db = DynamoDB(table_name)
 
     def get_debt(self, user_id1, user_id2):
         # user_id1からuser_id2への借金の量を取得します。
-        pass
+        key = {
+            'user_id1': user_id1,
+            'user_id2': user_id2
+        }
+        item = self.dynamo_db.get_item(key)
+        return item.get('debt', 0)
 
     def set_debt(self, user_id1, user_id2, amount):
         # user_id1からuser_id2への借金の量を設定します。
-        pass
+        item = {
+            'user_id1': user_id1,
+            'user_id2': user_id2,
+            'debt': amount
+        }
+        self.dynamo_db.put_item(item)
 
     def add_debt(self, user_id1, user_id2, amount):
         # user_id1からuser_id2への借金の量を増やします。
-        pass
+        current_debt = self.get_debt(user_id1, user_id2)
+        new_debt = current_debt + amount
+        self.set_debt(user_id1, user_id2, new_debt)
 
     def reduce_debt(self, user_id1, user_id2, amount):
         # user_id1からuser_id2への借金の量を減らします。
-        pass
+        current_debt = self.get_debt(user_id1, user_id2)
+        new_debt = current_debt - amount
+        self.set_debt(user_id1, user_id2, new_debt)
